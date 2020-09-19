405641f85b624cc5b836b547aff3ba94fd466fe2
Shreya <Shreyaroxx777@gmail.com>
2020-09-19 08:32:16 +0530

    Upadte speech.js

--- Speech/speech.js
+++ Speech/speech.js
@@ -2,19 +2,36 @@ var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
 
+var database = ['electron', 'proton', 'neutron', 'atom', 'tissues', 'plant', 'animal'] 
+var grammar = '#JSGF V1.0; grammar database;'
+var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+speechRecognitionList.addFromString(grammar, 1);
+recognition.grammars = speechRecognitionList;
+recognition.continuous = false;
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+window.setInterval(stt, 10);
+
+//Jquery
 var noteTextarea = $('#note-textarea');
 var instructions = $('#recording-instructions');
 var notesList = $('ul#notes');
-
 var noteContent = '';
 
-// Get all notes from previous sessions and display them.
+// previous sesh notes display ()
 var notes = getAllNotes();
 renderNotes(notes);
+
+
 
   // Text to speech
   function readOutLoud(message) {
     var speech = new SpeechSynthesisUtterance();
+
+  // Set the text and voice attributes.
     speech.text = message;
     speech.volume = 1;
     speech.rate = 1;

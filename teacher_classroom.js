c1022177c83fed5ce20a97976b8b7e98c6ef6af2
Yash.S.Narang <y_narang@yahoo.in>
2020-09-20 02:14:40 +0530

    Message load done

--- teacher_classroom.js
+++ teacher_classroom.js
@@ -12,6 +12,20 @@ window.addEventListener("DOMContentLoaded", (e) => {
   const urlParams = new URLSearchParams(window.location.search);
   const myParam = urlParams.get("id");
   console.log(myParam);
+  db.collection("messages")
+    .doc(myParam)
+    .get()
+    .then((snapshot) => {
+      const data = snapshot.data();
+      for (key in data) {
+        const div = document.createElement("div");
+        div.className = "callout callout-primary";
+        div.style = "border-radius:20px;";
+        div.innerHTML = `<h4>Faculty - ${data[key].timestamp}</h4>${data[key].message}`;
+        console.log(div);
+        document.getElementById("message-container").appendChild(div);
+      }
+    });
   send_button = document.getElementById("msg_send");
   send_button.addEventListener("click", (e) => {
     e.preventDefault();

e38e02c3fe23fcc8c40787dd45f74cba8df0032e
Shreya <Shreyaroxx777@gmail.com>
2020-09-19 10:13:00 +0530

    Updates speech.js

--- Speech/speech.js
+++ Speech/speech.js
@@ -1,66 +1,161 @@
-var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
-var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
-var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
-
-var database = ['electron', 'proton', 'neutron', 'atom', 'tissues', 'plant', 'animal'] 
-var grammar = '#JSGF V1.0; grammar database;'
-var recognition = new SpeechRecognition();
-var speechRecognitionList = new SpeechGrammarList();
-speechRecognitionList.addFromString(grammar, 1);
-recognition.grammars = speechRecognitionList;
-recognition.continuous = false;
-recognition.lang = 'en-US';
-recognition.interimResults = false;
-recognition.maxAlternatives = 1;
-
-window.setInterval(stt, 10);
-
-//Jquery
-var noteTextarea = $('#note-textarea');
-var instructions = $('#recording-instructions');
-var notesList = $('ul#notes');
-var noteContent = '';
-
-// previous sesh notes display ()
-var notes = getAllNotes();
-renderNotes(notes);
-
-// Speech recog
-
-function stt(){
-    recognition.start();
-    console.log(' Speech function working');
-
-    recognition.onresult = function(event) {
-        var word = event.results[current][0].transcript;
-
-        var mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
+try {
+    var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    var recognition = new SpeechRecognition();
+  }
+  catch(e) {
+    console.error(e);
+    $('.no-browser-support').show();
+    $('.app').hide();
+  }
   
-        if(!mobileRepeatBug) {
-        noteContent += transcript;
-        noteTextarea.val(noteContent);
-        }
-        noteTextarea.textContent = 'Result received: ' + word + '.';
-        console.log('Confidence: ' + event.results[0][0].confidence + word);
-    }
-    recognition.onstart = function() { 
-        instructions.text('Voice recognition activated. Try speaking into the microphone.');
-      }
-      
-      recognition.onspeechend = function() {
-        instructions.text('You were quiet for a while so voice recognition turned itself off.');
-      }
-      
-      recognition.onerror = function(event) {
-        if(event.error == 'no-speech') {
-          instructions.text('No speech was detected. Try again.');  
-
-        };
-      }
+  
+  var noteTextarea = $('#note-textarea');
+  var instructions = $('#recording-instructions');
+  var notesList = $('ul#notes');
+  
+  var noteContent = '';
+  
+  // Get all notes from previous sessions and display them.
+  var notes = getAllNotes();
+  renderNotes(notes);
+  
+  
+  
+  /*-----------------------------
+        Voice Recognition 
+  ------------------------------*/
+  
+  // If false, the recording will stop after a few seconds of silence.
+  // When true, the silence period is longer (about 15 seconds),
+  // allowing us to keep recording even when the user pauses. 
+  recognition.continuous = true;
+  
+  // This block is called every time the Speech APi captures a line. 
+  recognition.onresult = function(event) {
+  
+    // event is a SpeechRecognitionEvent object.
+    // It holds all the lines we have captured so far. 
+    // We only need the current one.
+    var current = event.resultIndex;
+  
+    // Get a transcript of what was said.
+    var transcript = event.results[current][0].transcript;
+  
     
-}
-
-function renderNotes(notes) {
+  
+    // Add the current transcript to the contents of our Note.
+    // There is a weird bug on mobile, where everything is repeated twice.
+    // There is no official solution so far so we have to handle an edge case.
+    var mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
+  
+    if(!mobileRepeatBug) {
+      noteContent += transcript;
+      noteTextarea.val(noteContent);
+    }
+  };
+  
+  recognition.onstart = function() { 
+    instructions.text('Voice recognition activated. Try speaking into the microphone.');
+  }
+  
+  recognition.onspeechend = function() {
+    instructions.text('You were quiet for a while so voice recognition turned itself off.');
+  }
+  
+  recognition.onerror = function(event) {
+    if(event.error == 'no-speech') {
+      instructions.text('No speech was detected. Try again.');  
+    };
+  }
+  
+  
+  
+  /*-----------------------------
+        App buttons and input 
+  ------------------------------*/
+  
+  $('#start-record-btn').on('click', function(e) {
+    if (noteContent.length) {
+      noteContent += ' ';
+    }
+    recognition.start();
+  });
+  
+  
+  $('#pause-record-btn').on('click', function(e) {
+    recognition.stop();
+    instructions.text('Voice recognition paused.');
+  });
+  
+  // Sync the text inside the text area with the noteContent variable.
+  noteTextarea.on('input', function() {
+    noteContent = $(this).val();
+  })
+  
+  $('#save-note-btn').on('click', function(e) {
+    recognition.stop();
+  
+    if(!noteContent.length) {
+      instructions.text('Could not save empty note. Please add a message to your note.');
+    }
+    else {
+      // Save note to localStorage.
+      // The key is the dateTime with seconds, the value is the content of the note.
+      saveNote(new Date().toLocaleString(), noteContent);
+  
+      // Reset variables and update UI.
+      noteContent = '';
+      renderNotes(getAllNotes());
+      noteTextarea.val('');
+      instructions.text('Note saved successfully.');
+    }
+        
+  })
+  
+  
+  notesList.on('click', function(e) {
+    e.preventDefault();
+    var target = $(e.target);
+  
+    // Listen to the selected note.
+    if(target.hasClass('listen-note')) {
+      var content = target.closest('.note').find('.content').text();
+      readOutLoud(content);
+    }
+  
+    // Delete note.
+    if(target.hasClass('delete-note')) {
+      var dateTime = target.siblings('.date').text();  
+      deleteNote(dateTime);
+      target.closest('.note').remove();
+    }
+  });
+  
+  
+  
+  /*-----------------------------
+        Speech Synthesis 
+  ------------------------------*/
+  
+  function readOutLoud(message) {
+      var speech = new SpeechSynthesisUtterance();
+  
+    // Set the text and voice attributes.
+      speech.text = message;
+      speech.volume = 1;
+      speech.rate = 1;
+      speech.pitch = 1;
+    
+      window.speechSynthesis.speak(speech);
+  }
+  
+  
+  
+  /*-----------------------------
+        Helper Functions 
+  ------------------------------*/
+  
+  function renderNotes(notes) {
     var html = '';
     if(notes.length) {
       notes.forEach(function(note) {
@@ -109,65 +204,3 @@ function renderNotes(notes) {
   function deleteNote(dateTime) {
     localStorage.removeItem('note-' + dateTime); 
   }
-//DOM manupulation
-
-$('#start-record-btn').on('click', function(e) {
-    if (noteContent.length) {
-      noteContent += ' ';
-    }
-    recognition.start();
-  });
-  
-  
-  $('#pause-record-btn').on('click', function(e) {
-    recognition.stop();
-    instructions.text('Voice recognition paused.');
-  });
-  
-  noteTextarea.on('input', function() {
-    noteContent = $(this).val();
-  })
-  
-  $('#save-note-btn').on('click', function(e) {
-    recognition.stop();
-  
-    if(!noteContent.length) {
-      instructions.text('Could not save empty note. Please add a message to your note.');
-    }
-    else {
-      saveNote(new Date().toLocaleString(), noteContent);
-      noteContent = '';
-      renderNotes(getAllNotes());
-      noteTextarea.val('');
-      instructions.text('Note saved successfully.');
-    }
-        
-  })
-   
-  notesList.on('click', function(e) {
-    e.preventDefault();
-    var target = $(e.target);
-    if(target.hasClass('listen-note')) {
-      var content = target.closest('.note').find('.content').text();
-      readOutLoud(content);
-    }
-    if(target.hasClass('delete-note')) {
-      var dateTime = target.siblings('.date').text();  
-      deleteNote(dateTime);
-      target.closest('.note').remove();
-    }
-  });
-  
-
-  // Text to speech
-  function readOutLoud(message) {
-    var speech = new SpeechSynthesisUtterance();
-
-  // Set the text and voice attributes.
-    speech.text = message;
-    speech.volume = 1;
-    speech.rate = 1;
-    speech.pitch = 1;
-  
-    window.speechSynthesis.speak(speech);
-}

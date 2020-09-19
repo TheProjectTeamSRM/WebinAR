7f8a08e776195c6bbcb02ef41187ee2d7d9fd43b
Yash.S.Narang <y_narang@yahoo.in>
2020-09-19 22:01:36 +0530

    Faculty login done

--- faculty-login.js
+++ faculty-login.js
@@ -1,1 +1,19 @@
-const signUp=document.getElementById()
+window.addEventListener("DOMContentLoaded", () => {
+  let loginBtn = document.getElementById("loginBtn");
+  loginBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const email = document.getElementById("inputEmail").value;
+    const pass = document.getElementById("inputPassword").value;
+    auth
+      .signInWithEmailAndPassword(email, pass)
+      .then((cred) => {
+        window.location.replace("home-teachers.html");
+      })
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorMessage);
+      });
+  });
+});

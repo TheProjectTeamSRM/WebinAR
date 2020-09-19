98e6b7c051565ff0d7b288c236788bedbabda132
Eshaan-B <eb7127@srmist.edu.in>
2020-09-19 14:18:07 +0530

    Auth added to faculty and student

--- app.js
+++ app.js
@@ -1,42 +1,28 @@
-const flogin=document.getElementById('fLogin');
 
-const provider = new firebase.auth.GoogleAuthProvider();
+//auth.currentUser
 
-
+//on user changes
 auth.onAuthStateChanged(user=>{    
     if(user!=null)
     {
-        console.log(user);
+        //Updating the teacher/student status
+        
+        if (user != null) {
+            user.providerData.forEach(function (profile) {
+              console.log("Sign-in provider: " + profile.providerId);
+              console.log("  Provider-specific UID: " + profile.uid);
+              console.log("  Name: " + profile.displayName);
+              console.log("  Email: " + profile.email);
+              console.log("  Photo URL: " + profile.photoURL);
+            });
+          }
+        
+        //console.log(provider.Fb.type);
     }
     else
     {
         console.log('User logged out');
     }
-})
-flogin.addEventListener('click',function(){
-    provider.setCustomParameters({
-        'type': 'faculty'
-      });
-    
-    auth.signInWithPopup(provider).then(function(cred) {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = cred.credential.accessToken;
-      // The signed-in user info.
-      var user = cred.user;
-      console.log(user);
-      console.log(provider.Fb.type);
-      window.location.replace("home-teachers.html");
-      // ...
-    }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorMessage);  
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
-    });
+});
 
-})
 
